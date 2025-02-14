e1bb069a3cf2beb11e9bc6f02c1ec22296f1174b
ErikaFurudo111297 <148293084+ErikaFurudo111297@users.noreply.github.com>
2025-02-14 10:38:46 +0000

    Add files via upload

---
+++ Matrix.java
@@ -0,0 +1,11 @@
+public class Matrix {
+    public static void main(String[] Args) {
+        int[] Array = {1,2,3,4};
+        System.out.println("1 2 3 4");
+        System.out.print("1");
+        System.out.print(" 2");
+        System.out.print(" 3");
+        System.out.print(" 4");
+        System.out.printf("%n%d %d %d %d", Array[0], Array[1], Array[2], Array[3]);
+    }
+}
